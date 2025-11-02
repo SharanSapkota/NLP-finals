68b823de3c0c0bb8e4da6b36bab6bf8accc309bf
Sharan Sapkota <sapkotarambbo@gmail.com>
2025-11-02 07:39:21 +0200

    Make code runnable independently

--- task4_entities.py
+++ task4_entities.py
@@ -1,4 +1,3 @@
-# task4_entities.py
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
@@ -10,3 +9,12 @@ def compute_tfidf_entities_similarity(wiki_data):
     tfidf_matrix = vectorizer.fit_transform(preprocessed_entities.values())
     cos_sim = cosine_similarity(tfidf_matrix)
     return pd.DataFrame(cos_sim, index=wiki_data.keys(), columns=wiki_data.keys())
+
+if __name__ == "__main__":
+    import task1_wiki as t1
+    wiki_data = {k: t1.get_wiki_content(k) for k in t1.keywords}
+
+    tfidf_entities_sim = compute_tfidf_entities_similarity(wiki_data)
+    print("TF-IDF Cosine Similarity (Entities):")
+    print(tfidf_entities_sim)
+
